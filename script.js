ffd1b75ecf121d4a54a89229b36f319227b5fd39
Chakshu1760 <charliealawadhi@gmail.com>
2024-02-14 18:00:33 -0500

    update

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      items:1 
-    });
+  $(".owl_carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:2
+      },
+      
+    }
   });
-  
+});
